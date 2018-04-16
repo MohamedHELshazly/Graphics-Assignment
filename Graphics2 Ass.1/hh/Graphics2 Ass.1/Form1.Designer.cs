36f015a64c604398d4d0645a64ff26af57ebd0bb
MohamedHELshazly <37432361+MohamedHELshazly@users.noreply.github.com>
2018-04-17 00:42:16 +0200

    Add files via upload

---
+++ Graphics2 Ass.1/hh/Graphics2 Ass.1/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Graphics2_Ass._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1192, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Rot Z";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1023, 286);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(44, 260);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1111, 41);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Draw";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(1073, 286);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(45, 260);
+            this.listBox2.TabIndex = 7;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(1126, 286);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(49, 260);
+            this.listBox3.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1111, 14);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1192, 97);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "RotX";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1273, 98);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "RotY";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1192, 41);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 52);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Rot Z back";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1192, 124);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 52);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Rot X back";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1273, 125);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 52);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Rot Y back";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(1273, 14);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 79);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "Browse Edges";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(1181, 286);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(177, 260);
+            this.listBox4.TabIndex = 16;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1178, 191);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(89, 42);
+            this.button10.TabIndex = 17;
+            this.button10.Text = "Left";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(1273, 191);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(87, 42);
+            this.button11.TabIndex = 18;
+            this.button11.Text = "Right";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(1111, 89);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 41);
+            this.button12.TabIndex = 19;
+            this.button12.Text = "Calculate";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 550);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Graphics2 Ass.1/hh/Graphics2 Ass.1/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Graphics2_Ass._1
+{
+    public partial class Form1 : Form
+    {
+        Timer tt = new Timer();
+        ArrayList points = new ArrayList();
+        ArrayList points2 = new ArrayList();
+        ArrayList Edges = new ArrayList();
+        int Flag_Edge = 0;
+        Bitmap off_Image = null;
+        int flag = 0;
+        int driv = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            DB();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                //ptrv.x = (int)(ptrv.x * Math.Cos(0.1) - ptrv.y * Math.Sin(0.1));
+                //ptrv.y = (int)(ptrv.x * Math.Sin(0.1) + ptrv.y * Math.Cos(0.1));
+
+                double x = ptrv.x;
+                double y = ptrv.y;
+                x = (ptrv.x * Math.Cos(5 * Math.PI / 180) - ptrv.y * Math.Sin(5 * Math.PI / 180));
+                y = (ptrv.y * Math.Cos(5 * Math.PI / 180) + ptrv.x * Math.Sin(5 * Math.PI / 180));
+                ptrv.x = x;
+                ptrv.y = y;
+            }
+            DB();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (flag == 1)
+            {
+                flag = 1;
+            }
+            DB();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+            string S;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    S = sr.ReadLine();
+                    S.Trim();
+                    //textBox1.Text = S.ToString();
+
+                    string[] s = S.Split(',');
+                    int x, y, z;
+                    int.TryParse(s[0], out x);
+                    int.TryParse(s[1], out y);
+                    int.TryParse(s[2], out z);
+                    cnode n = new cnode();
+                    n.x = x;
+                    n.y = y;
+                    n.z = z;
+                    n.dri = i;
+                    points.Add(n);
+                    i++;
+                    //listBox1.Items.Add(S.ToString());
+                }
+                sr.Close();
+            }
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                //textBox1.Text = ptrv.x.ToString();
+                ptrv.x += this.ClientSize.Width / 2;
+                cnode n = new cnode();
+                n.x = ptrv.x;
+                n.y = ptrv.y;
+                points2.Add(n);
+                listBox1.Items.Add(ptrv.x.ToString());
+                listBox2.Items.Add(ptrv.y.ToString());
+                listBox3.Items.Add(ptrv.z.ToString());
+            }
+        }
+        void DrawScene(Graphics g)
+        {
+            Pen pe = new Pen(Color.Black);
+            g.Clear(Color.Gray);
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+
+                g.FillEllipse(Brushes.Black, (float)ptrv.x, (float)ptrv.y, 5, 5);
+            }
+
+            if (Flag_Edge == 1)
+            {
+                for (int i = 0; i < Edges.Count; i++)
+                {
+                    cnode ptrv = (cnode)Edges[i];
+                    cnode pt = (cnode)points[(int)ptrv.x];
+                    cnode pt2 = (cnode)points[(int)ptrv.y];
+                    g.DrawLine(Pens.Black, (float)pt.x, (float)pt.y, (float)pt2.x, (float)pt2.y);
+
+                    cnode ptm = (cnode)Edges[driv];
+                    cnode ptt = (cnode)points[(int)ptm.x];
+                    cnode ptt2 = (cnode)points[(int)ptm.y];
+                    g.DrawLine(Pens.Yellow, (float)ptt.x, (float)ptt.y, (float)ptt2.x, (float)ptt2.y);
+                }
+            }
+
+        }
+        void DB()
+        {
+
+            if (off_Image == null)
+            {
+                off_Image = new Bitmap(ClientRectangle.Width,
+                    ClientRectangle.Height);
+            }
+            Graphics g = this.CreateGraphics();
+            /////////////////////////////////////
+            ///	Get the Memory Graphics of the 
+            ///	BakcBuffer Image,
+            ///	/////////////////////////////////
+            Graphics g2 = Graphics.FromImage(off_Image);
+
+            //////////////////////////////////////
+            //	call our isolated drawing routing
+            //  but pass to it g2 (BackBuffer)
+            //  so the rendering will be done in
+            //	the memory.
+            //////////////////////////////////////
+            DrawScene(g2);
+
+
+            ////////////////////////////////////////
+            //	push our bitmap forward to the screen
+            ////////////////////////////////////////
+            g.DrawImage(off_Image, 0, 0);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                double z = ptrv.z;
+                double y = ptrv.y;
+                z = (ptrv.z * Math.Cos(5 * Math.PI / 180) - ptrv.y * Math.Sin(5 * Math.PI / 180));
+                y = (ptrv.y * Math.Cos(5 * Math.PI / 180) + ptrv.z * Math.Sin(5 * Math.PI / 180));
+                ptrv.z = z;
+                ptrv.y = y;
+            }
+            DB();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                double z = ptrv.z;
+                double x = ptrv.x;
+                z = (ptrv.z * Math.Cos(5 * Math.PI / 180) + ptrv.x * Math.Sin(5 * Math.PI / 180));
+                x = (ptrv.x * Math.Cos(5 * Math.PI / 180) - ptrv.z * Math.Sin(5 * Math.PI / 180));
+                ptrv.z = z;
+                ptrv.x = x;
+            }
+            DB();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+
+                double x = ptrv.x;
+                double y = ptrv.y;
+                x = (ptrv.x * Math.Cos(5 * Math.PI / 180) - ptrv.y * Math.Sin(5 * Math.PI / 180));
+                y = (ptrv.y * Math.Cos(5 * Math.PI / 180) + ptrv.x * Math.Sin(5 * Math.PI / 180));
+                ptrv.x = x;
+                ptrv.y = y;
+            }
+            DB();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                double z = ptrv.z;
+                double y = ptrv.y;
+                z = (ptrv.z * Math.Cos(5 * Math.PI / 180) + ptrv.y * Math.Sin(5 * Math.PI / 180));
+                y = (ptrv.y * Math.Cos(5 * Math.PI / 180) - ptrv.z * Math.Sin(5 * Math.PI / 180));
+                ptrv.z = z;
+                ptrv.y = y;
+            }
+            DB();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                double z = ptrv.z;
+                double x = ptrv.x;
+                z = (ptrv.z * Math.Cos(5 * Math.PI / 180) + ptrv.x * Math.Sin(5 * Math.PI / 180));
+                x = (ptrv.x * Math.Cos(5 * Math.PI / 180) - ptrv.z * Math.Sin(5 * Math.PI / 180));
+                ptrv.z = z;
+                ptrv.x = x;
+            }
+            DB();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Flag_Edge = 1;
+            string S;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+
+                while (!sr.EndOfStream)
+                {
+                    S = sr.ReadLine();
+                    S.Trim();
+                    //textBox1.Text = S.ToString();
+
+                    string[] s = S.Split(',');
+                    int x, y;
+                    int.TryParse(s[0], out x);
+                    int.TryParse(s[1], out y);
+                    cnode n = new cnode();
+                    n.x = x;
+                    n.y = y;
+                    Edges.Add(n);
+                    //listBox1.Items.Add(S.ToString());
+                }
+                sr.Close();
+            }
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                cnode ptrv = (cnode)Edges[i];
+                listBox4.Items.Add(ptrv.x.ToString());
+                listBox4.Items.Add(ptrv.y.ToString());
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (driv > 0)
+                driv--;
+            DB();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (driv < points.Count)
+                driv++;
+            DB();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            cnode ptr = (cnode)Edges[driv];
+            cnode pt = (cnode)points[(int)ptr.x];
+            cnode pt2 = (cnode)points[(int)ptr.y];
+            //double Delta = Math.Sqrt(Math.Pow((pt2.y - pt.y), 2) + Math.Pow((pt2.x - pt.x), 2) + Math.Pow((pt2.z - pt.z), 2));
+            //double x = Math.Sqrt(Math.Pow((pt2.x - pt.x), 2));
+            //double y = Math.Sqrt(Math.Pow((pt2.y - pt.y), 2));
+            //double z = Math.Sqrt(Math.Pow((pt2.z - pt.z), 2));
+
+            double x = pt2.x - pt.x;
+            double y = pt2.y - pt.y;
+            double z = pt2.z - pt.z;
+
+            double x2 = pt.x;
+            double y2 = pt.y;
+            double z2 = pt.z;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode p1;
+                p1 = (cnode)points[i];
+
+                p1.x -= x2;
+                p1.y -= y2;
+                p1.z -= z2;
+
+                points[i] = p1;
+            }
+
+
+
+            double th = Math.Atan2(y, x);
+            th = th * Math.PI / 180;
+
+            double phi = Math.Atan2(Math.Sqrt(x * x + y * y), z);
+            phi = (phi * Math.PI / 180);
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                    cnode ptrv = (cnode)points[i];
+
+                    double xx = ptrv.x;
+                    double yy = ptrv.y;
+                    
+                    xx = ((ptrv.x * Math.Cos(-th)) - (ptrv.y * Math.Sin(-th)));
+                    yy = ((ptrv.x * Math.Sin(-th)) + (ptrv.y * Math.Cos(-th)));
+                    
+                    ptrv.x = xx;
+                    ptrv.y = yy;
+                    points[i] = ptrv;                
+            }
+
+
+
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                
+                double zz = ptrv.z;
+                double xx = ptrv.x;
+                
+                zz = ((ptrv.x * Math.Cos(-(phi))) + (ptrv.z * Math.Sin(-(phi))));
+                xx = ((ptrv.z * Math.Cos(-(phi))) - (ptrv.x * Math.Sin(-(phi))));
+
+//                zz = ((ptrv.x * Math.Sin(-(phi))) + (ptrv.z * Math.Cos(-(phi))));
+//                xx = -((ptrv.z * Math.Sin(-(phi))) + (ptrv.x * Math.Cos(-(phi))));
+
+                ptrv.z = zz;
+                ptrv.x = xx;
+
+                points[i] = ptrv;
+            }
+            
+
+
+
+           
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+
+                double xx = ptrv.x;
+                double yy = ptrv.y;
+                xx = ((ptrv.x * Math.Cos(0.20)) - (ptrv.y * Math.Sin(0.20)));
+                yy = ((ptrv.y * Math.Cos(0.20)) + (ptrv.x * Math.Sin(0.20)));
+                ptrv.x = xx;
+                ptrv.y = yy;
+
+                points[i] = ptrv;
+            }
+
+
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+                double zz = ptrv.z;
+                double xx = ptrv.x;
+                zz = ((ptrv.x * Math.Cos((phi))) + (ptrv.z * Math.Sin((phi))));
+                xx = ((ptrv.z * Math.Cos((phi))) - (ptrv.x * Math.Sin((phi))));
+
+//                zz = ((ptrv.x * Math.Sin(-(phi))) + (ptrv.z * Math.Cos(-(phi))));
+//                xx = -((ptrv.z * Math.Sin(-(phi))) + (ptrv.x * Math.Cos(-(phi))));
+
+                ptrv.z = zz;
+                ptrv.x = xx;
+
+                points[i] = ptrv;
+            }
+
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode ptrv = (cnode)points[i];
+
+                double xx = ptrv.x;
+                double yy = ptrv.y;
+                xx = ((ptrv.x * Math.Cos(th)) - (ptrv.y * Math.Sin(th)));
+                yy = ((ptrv.y * Math.Cos(th)) + (ptrv.x * Math.Sin(th)));
+                ptrv.x = xx;
+                ptrv.y = yy;
+
+                points[i] = ptrv;
+            }
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                cnode p1 = new cnode();
+                p1 = (cnode)points[i];
+                p1.x += x2;
+                p1.y += y2;
+                p1.z += z2;
+
+                points[i] = p1;
+            }
+            DB();
+        }
+    }
+    class cnode
+    {
+        public double x, y, z, dri;
+    }
+}
